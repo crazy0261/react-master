a78682c5742a874f41dd981ed049f00ca56f43c2
menghuiding <dingmenghui026@163.com>
2023-04-22 16:25:42 +0800

    feat: request 封装 table获取请求数据信息并渲染到列表中

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    // host： '0.0.0.0'， 使用当前的ip地址，没有这个就是以localhost作为本地地址
+    // port：3000 端口号为3000
+    open: false,  // 是否在默认浏览器中自动打开该地址
+    proxy: { // 使用代理
+      '/api': { // 当有 /api开头的地址是 ，代理到target地址
+        target: 'http://localhost:5678', // 需要跨域代理到本地路径
+        changeOrigin: true, // 是否改变请求源头
+        rewrite: (path) => path.replace(/^\/api/, '') // 路径重写
+      }
+    }
+  }
 })
